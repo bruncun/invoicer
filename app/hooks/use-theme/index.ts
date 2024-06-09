eba605cb9380857a4a972f83259792229816dfb9
Bruno Cunha <bruncun@icloud.com>
2024-06-08 21:09:23 -0400

    update auth forms

--- app/hooks/use-theme/index.ts
+++ app/hooks/use-theme/index.ts
@@ -1,24 +1,12 @@
-import { useState, useLayoutEffect, useEffect } from "react";
+import { useState, useLayoutEffect } from "react";
 
 const useTheme = () => {
-  const getStoredTheme = (): string | null =>
-    typeof window !== "undefined" ? localStorage.getItem("theme") : null;
-  const setStoredTheme = (theme: string): void => {
-    if (typeof window !== "undefined") {
-      localStorage.setItem("theme", theme);
-    }
-  };
-  const getPreferredTheme = (): string => {
-    const storedTheme = getStoredTheme();
-    if (storedTheme) {
-      return storedTheme;
-    }
-    return typeof window !== "undefined" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches
+  const getPreferredTheme = () =>
+    typeof window !== "undefined" &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches
       ? "dark"
       : "light";
-  };
-  const setTheme = (theme: string): void => {
+  const setTheme = (theme: string) => {
     if (typeof window !== "undefined") {
       if (theme === "auto") {
         document.documentElement.setAttribute(
@@ -33,20 +21,17 @@ const useTheme = () => {
     }
   };
   const [theme, setThemeState] = useState<string>(getPreferredTheme());
+
   useLayoutEffect(() => {
     setTheme(theme);
-    setStoredTheme(theme);
   }, [theme]);
-  useEffect(() => {
-    const storedTheme = getStoredTheme();
-    if (storedTheme && storedTheme !== theme) {
-      setThemeState(storedTheme);
-    }
-  }, []);
+
   const toggleTheme = () => {
     const newTheme = theme === "dark" ? "light" : "dark";
     setThemeState(newTheme);
   };
+
   return { theme, toggleTheme };
 };
+
 export default useTheme;
