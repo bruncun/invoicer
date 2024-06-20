76c4e894329841ac038c57496da923a358ca6e03
Bruno Cunha <bruncun@icloud.com>
2024-06-20 16:55:02 -0400

    add invoice list skeletons

--- app/components/invoices/list-group/index.tsx
+++ app/components/invoices/list-group/index.tsx
@@ -12,31 +12,36 @@ export const InvoicesListGroup = ({
 }: InvoicesListGroupProps) => {
   const invoices = data?.data as Array<Invoice>;
 
+  if (isLoading || isRefetching) {
+    return (
+      <ul
+        data-testid="invoices-list"
+        className="list-unstyled mt-3 mb-2 pb-2 d-flex flex-column gap-2"
+      >
+        {new Array(10).fill(null).map((invoice, idx) => (
+          <li key={idx}>
+            <InvoicesListItem invoice={invoice} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
+
   return (
     <>
-      {invoices?.length > 0 || isLoading || isRefetching ? (
+      {invoices?.length > 0 ? (
         <ul
           data-testid="invoices-list"
-          className="list-unstyled mt-3 mb-2 pb-2"
+          className="list-unstyled mt-3 mb-2 pb-2 d-flex flex-column gap-2"
         >
-          {invoices?.map((invoice, idx) => (
-            <li
-              key={invoice.id}
-              className={
-                idx !== invoices?.length - 1
-                  ? "my-2 border border-transparent"
-                  : ""
-              }
-            >
-              <InvoicesListItem
-                invoice={invoice}
-                isLoading={isLoading}
-              ></InvoicesListItem>
+          {invoices.map((invoice, idx) => (
+            <li key={invoice.id}>
+              <InvoicesListItem invoice={invoice} />
             </li>
           ))}
         </ul>
       ) : (
-        <InvoicesEmptyState></InvoicesEmptyState>
+        <InvoicesEmptyState />
       )}
     </>
   );
--- app/components/invoices/pager/index.tsx
+++ app/components/invoices/pager/index.tsx
@@ -1,13 +1,15 @@
 import { Button } from "react-bootstrap";
 import Icon from "~/components/icon";
+import Skeleton from "~/components/skeleton";
 import { InvoicesList } from "~/hooks/invoices/use-invoices-list";
+import InvoiceList from "~/routes/_layout.invoices._index";
 
 type InvoicesPagerProps = {
   invoicesList: InvoicesList;
 };
 
 export const InvoicesPager = ({
-  invoicesList: { pageSizeState, currentState, data },
+  invoicesList: { pageSizeState, currentState, data, isLoading, isRefetching },
 }: InvoicesPagerProps) => {
   const [pageSize] = pageSizeState;
   const [current, setCurrent] = currentState;
@@ -15,15 +17,21 @@ export const InvoicesPager = ({
   const isNextPageAvailable = total >= pageSize * current + 1;
   const formattedTotal = new Intl.NumberFormat("en-US").format(total);
 
-  if (total === 0) return null;
+  if (total === 0 && !isLoading) return null;
 
   return (
     <div className="d-flex justify-content-between align-items-center">
-      <span className="text-muted fs-6 d-none d-xl-inline-block">
-        Showing{" "}
-        <span className="fw-medium">{(current - 1) * pageSize + 1}</span> to{" "}
-        <span className="fw-medium">{current * pageSize}</span> of{" "}
-        <span className="fw-medium">{formattedTotal}</span> invoices
+      <span className="text-muted fs-6 d-none d-xl-inline-block lh-1">
+        {isLoading ? (
+          <Skeleton className="w-11 bg-secondary" style={{ width: "15rem" }} />
+        ) : (
+          <>
+            Showing{" "}
+            <span className="fw-medium">{(current - 1) * pageSize + 1}</span> to{" "}
+            <span className="fw-medium">{current * pageSize}</span> of{" "}
+            <span className="fw-medium">{formattedTotal}</span> invoices
+          </>
+        )}
       </span>
       <div className="d-flex justify-content-between w-100 d-xl-inline-block w-xl-auto">
         <Button
@@ -39,7 +47,7 @@ export const InvoicesPager = ({
           variant="link"
           className="rounded-3"
           onClick={() => setCurrent((prev) => prev + 1)}
-          disabled={!isNextPageAvailable}
+          disabled={!isNextPageAvailable || isLoading}
         >
           Next
           <Icon name="arrow-right text-primary ms-2"></Icon>
