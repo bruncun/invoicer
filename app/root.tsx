1978d23368c3c1b6d6b7f44532d1e705a490a7f7
Bruno Cunha <bruncun@icloud.com>
2024-06-28 19:47:00 -0400

    refactor root component to remove unused imports and refine-kbar provider

--- app/root.tsx
+++ app/root.tsx
@@ -9,10 +9,7 @@ import {
   useRouteError,
 } from "@remix-run/react";
 import { Refine } from "@refinedev/core";
-import { RefineKbarProvider } from "@refinedev/kbar";
-import routerProvider, {
-  UnsavedChangesNotifier,
-} from "@refinedev/remix-router";
+import routerProvider from "@refinedev/remix-router";
 import { dataProvider } from "@refinedev/supabase";
 import styles from "~/styles/index.css";
 import { supabaseClient } from "~/utility";
@@ -40,40 +37,37 @@ export default function App() {
         <style></style>
       </head>
       <body>
-        <RefineKbarProvider>
-          <Refine
-            routerProvider={routerProvider}
-            dataProvider={dataProvider(supabaseClient)}
-            authProvider={authProvider}
-            notificationProvider={notificationProvider}
-            resources={[
-              {
-                name: "invoices",
-                list: "/invoices",
-                create: "/invoices/create",
-                edit: "/invoices/edit/:id",
-                show: "/invoices/show/:id",
-                meta: {
-                  canDelete: true,
-                },
+        <Refine
+          routerProvider={routerProvider}
+          dataProvider={dataProvider(supabaseClient)}
+          authProvider={authProvider}
+          notificationProvider={notificationProvider}
+          resources={[
+            {
+              name: "invoices",
+              list: "/invoices",
+              create: "/invoices/create",
+              edit: "/invoices/edit/:id",
+              show: "/invoices/show/:id",
+              meta: {
+                canDelete: true,
               },
-            ]}
-            options={{
-              useNewQueryKeys: true,
-              projectId: "AGpg3C-5yTuaG-cOKhCy",
-            }}
-          >
-            <>
-              <Outlet />
-              <UnsavedChangesNotifier />
-              <ToastContainer
-                hideProgressBar
-                transition={None}
-                autoClose={5000}
-              />
-            </>
-          </Refine>
-        </RefineKbarProvider>
+            },
+          ]}
+          options={{
+            useNewQueryKeys: true,
+            projectId: "AGpg3C-5yTuaG-cOKhCy",
+          }}
+        >
+          <>
+            <Outlet />
+            <ToastContainer
+              hideProgressBar
+              transition={None}
+              autoClose={5000}
+            />
+          </>
+        </Refine>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
