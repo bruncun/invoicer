9e113e64f81e9abc394767a51a8c26781abd80ef
Bruno Cunha <bruncun@icloud.com>
2024-10-03 15:31:48 -0400

    reorganize constants

---
+++ app/constants/constants.ts
@@ -0,0 +1,21 @@
+import * as yup from "yup";
+import { Enums } from "~/types/supabase";
+
+yup.addMethod(yup.string, "postcode", function (message: string) {
+  return this.test("is-postcode", message, (value) => {
+    const postcodeRegex = /^[A-Za-z0-9]{3,}$/;
+    return postcodeRegex.test(value || "");
+  });
+});
+
+export const TOKEN_KEY = "supabase-token";
+export const STATUSES: Enums<"status">[] = ["draft", "pending", "paid"];
+
+export const validString = yup.string().trim();
+
+export const validEmail = validString.email("Invalid email address");
+
+export const constants = {
+  validString,
+  validEmail,
+};
